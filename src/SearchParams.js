e1c255c5605155ede024a9c15fd287838f3c0fa0
Shabab <shababsaifi@gmail.com>
2021-01-24 21:35:05 +0530

    added theme context

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,14 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import pet, { ANIMALS } from '@frontendmasters/pet';
 import useDropdown from './useDropdown';
 import Results from './Results';
+import ThemeContext from './ThemeContext';
 const SearchParams = () => {
   const [location, setLocation] = useState('Seattle, WA');
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown('Breed', '', breeds);
   const [pets, setPets] = useState([]);
-
+  const [theme, setTheme] = useContext(ThemeContext);
   useEffect(() => {
     setBreeds([]);
     setBreed('');
@@ -45,7 +46,19 @@ const SearchParams = () => {
         </label>
         <AnimalDropdown />
         <BreedDropdown />
-        <button>Submit</button>
+        <label htmlFor="theme">
+          <select
+            value={theme}
+            onChange={(e) => setTheme(e.target.value)}
+            onBlur={(e) => setTheme(e.target.value)}
+          >
+            <option value="red">Red</option>
+            <option value={theme}>{theme}</option>
+            <option value="pink">Pink</option>
+            <option value="yellow">Yellow</option>
+          </select>
+        </label>
+        <button style={{ backgroundColor: theme }}>Submit</button>
       </form>
       <Results pets={pets} />
     </div>
